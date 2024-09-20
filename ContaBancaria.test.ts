698b4c33a5876dbe3d5a1f264a3a6f1a116012ff
chiquinnho <capic.2024118tads0036@aluno.ifpi.edu.br>
2024-09-20 00:51:43 -0300

    Atrasado de novo

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,43 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { beforeEach, describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
 
+
+describe('ContaBancaria', () => {
+    let conta: ContaBancaria;
+    let conta2: ContaBancaria
+    let senha: ContaBancaria
+    beforeEach(() => {
+        conta = new ContaBancaria();
+        conta2 = new ContaBancaria();
+        senha = new ContaBancaria();
+    })
+
+    test("Testando depositar", () => {
+        conta.depositar(500)
+        expect(conta.depositar(500)).toBe(1000);
+    })
+
+    test('Testando saque', () => {
+        conta.depositar(400)
+        expect(conta.sacar(400)).toBe(0)
+    })
+
+    test('Testando transferirir.', () => {
+        conta.depositar(20) 
+        expect(conta.transferir(5, conta2)).toBe(15)
+        expect(conta2.transferir(5, conta)).toBe(0)
+        expect(conta.transferir(20, conta2)).toBe(0)
+        expect(conta2.transferir(20, conta)).toBe(0)
+    })
+
+    test('Exibindo Extrato', () => {
+        conta.exibirExtrato()
+        conta2.exibirExtrato()
+    })
+
+    test('Exibindo registros', () => {
+        conta.depositar(200)
+        expect(conta.registrarOperacao(""))
+    })
+})
 
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,71 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    private numeroConta: number
+    private agencia: number
+    private saldo: number
+    private extrato: string[]
+
+    public constructor(){
+        this.numeroConta = 0
+        this.agencia = 0
+        this.saldo = 0
+        this.extrato = []
+    }
+    
+    public depositar(valor: number) {
+        if(valor > 0){
+            this.saldo += valor
+            this.extrato.push(`Deposito de R$ ${valor}`)
+            this.registrarOperacao(`Valor depositado R$ ${valor}.`)
+            return this.saldo
+
+        } else {
+            console.log('Valor inválido para deposito.')
+        }
+    }
+
+    // Sò diminue se o valo for menor ou igual ao saldo
+
+    public sacar(valor: number) {
+        if (valor <= this.saldo && valor > 0) {
+            this.saldo -= valor
+            this.extrato.push(`Saque de R$${valor}.`)
+            this.registrarOperacao(`O saque de R$ ${valor}.`)
+            return this.saldo           
+        } else {
+            console.log(`Valor inválido para saque. Tá querendo me roubar!?`)
+        }
+    }
+
+    public transferir(valor:number, contaDestino: ContaBancaria) {
+        if (valor <= this.saldo && valor > 0) {
+            this.saldo -= valor
+            this.registrarOperacao(`Transferência de R$${valor}`)
+            contaDestino.receberTransferencia(valor, this.numeroConta)
+            return this.saldo
+        } else {
+            console.log(`Valor inválido para tranferência. Tá querendo me roubar!?`)
+        }
+    }
+
+    public receberTransferencia(valor: number, contaOrigem: number) {
+        this.saldo += valor;
+        this.extrato.push(`Recebimento de transferência de R$ ${valor} da conta ${contaOrigem}`);
+    }
+
+    public consultarSaldo() {
+        return this.saldo;
+    }
+
+    public exibirExtrato() {
+        return this.extrato;
+    }
+
+    // Método privado para registrar cada operação no extrato da conta, incluindo a data e a descrição da transação.
+    // acho que esse vai ter uma especie de login, porque é privado, com data, hora e descrição
+    
+    public registrarOperacao(descricao: string) {
+        const dataAtual = new Date().toLocaleString()
+        this.extrato.push(`${dataAtual} - ${descricao}`)
+
+    }
+}
